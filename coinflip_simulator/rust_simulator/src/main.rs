8863bfcf7e1cd0feba46ba019e9c15ee3c7c019d
Sebastien Ollquist <sebastienollquist@sebastiens-laptop.home>
2024-04-11 18:43:15 +0200

    optimised sequence generation with map

--- coinflip_simulator/rust_simulator/src/main.rs
+++ coinflip_simulator/rust_simulator/src/main.rs
@@ -1,3 +1,5 @@
+use rand::Rng;
+
 const PROB: f64 = 0.4;
 
 /// Flips a biased coin once and returns the result.
@@ -5,8 +7,8 @@ const PROB: f64 = 0.4;
 /// Returns:
 /// 0 if the coin lands on tails,
 /// 1 otherwise.
-fn flip_biased_coin() -> i32 {
-    return if rand::random::<f64>() < PROB { 0 } else { 1 };
+fn flip_biased_coin(rng: &mut impl Rng) -> i32 {
+    return if rng.gen::<f64>() < PROB { 0 } else { 1 };
 }
 
 /// Uses the Von Neumann method to generate an unbiase run of coin flips.
@@ -15,10 +17,10 @@ fn flip_biased_coin() -> i32 {
 /// Returns:
 /// 0 if the run is HT,
 /// 1 if the run is TH.
-fn get_unbiased_run() -> i32 {
+fn get_unbiased_run(rng: &mut impl Rng) -> i32 {
     loop {
-        let first = flip_biased_coin();
-        let second = flip_biased_coin();
+        let first = flip_biased_coin(rng);
+        let second = flip_biased_coin(rng);
         if first != second {
             return second;
         }
@@ -32,18 +34,15 @@ fn get_unbiased_run() -> i32 {
 /// 
 /// Returns:
 /// The number of heads in the sequence.
-fn generate_unbiased_sequence(length: i32) -> i32 {
-    let mut count = 0;
-    for _ in 0..length {
-        count += get_unbiased_run();
-    }
-    return count;
+fn generate_unbiased_sequence(length: i32, rng: &mut impl Rng) -> i32 {
+    return (0..length).map(|_| get_unbiased_run(rng)).sum();
 }
 
 /// Main program.
 fn main() {
+    let mut rng = rand::thread_rng(); // Use a specific RNG instance
     let start_time = std::time::Instant::now();
-    let total_heads = generate_unbiased_sequence(1e6 as i32);
+    let total_heads = generate_unbiased_sequence(1e6 as i32, &mut rng);
     let end_time = std::time::Instant::now();
     println!("Total heads: {}", total_heads);
     println!("Rust Time: {} [s]", (end_time - start_time).as_secs_f64());
