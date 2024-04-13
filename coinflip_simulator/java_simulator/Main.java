23c2726401e42245ea4b0e909d12fe305236fe79
Sebastien Ollquist <seboll@sebastiens-mac-mini-8.home>
2024-04-13 13:34:40 +0200

    added java structure

---
+++ coinflip_simulator/java_simulator/Main.java
@@ -0,0 +1,11 @@
+package coinflip_simulator.java_simulator;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
+public class Main {
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
+    public static void main(String[] args) {
+        logger.setLevel(Level.INFO);
+        logger.info("Hello, World!");
+    }
+}
